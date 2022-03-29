648af2a1c3deb9d6e798ac1819de76dddf84a057
Your <tolupumpay@gmail.com>
2022-03-29 01:59:53 +0100

    revised java project

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// print out//
+        System.out.println("1. Tolu Is A Boy");
+        System.out.println("2. Onene is my private tutor");
+        System.out.println("3. 2face is African's best ");
+        System.out.println("4. God is the greatest");
+        System.out.println("5. Life is a teacher");
+    }
+}
